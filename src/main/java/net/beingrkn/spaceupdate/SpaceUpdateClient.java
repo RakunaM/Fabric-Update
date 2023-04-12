3e33266cf385a24ead9e08cd813497929fa64c08
RKNpy <rkn412444@gmail.com>
2023-04-12 13:37:45 +0530

    Initial commit

---
+++ src/main/java/net/beingrkn/spaceupdate/SpaceUpdateClient.java
@@ -0,0 +1,10 @@
+package net.beingrkn.spaceupdate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpaceUpdateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
